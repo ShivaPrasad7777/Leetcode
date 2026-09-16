5d40c90d0a78088163c06a83f37dc03abf4ceb5e
ShivaPrasad7777 <pattedashivaprasad@gmail.com>
2026-09-16 12:06:32 +0530

    Time: 1 ms (98.45%) | Memory: 43.8 MB (6.39%) - LeetSync

--- 812-rotate-string/rotate-string.java
+++ 812-rotate-string/rotate-string.java
@@ -3,10 +3,18 @@ class Solution {
         if(s.length() != goal.length()){
             return false;
         }
-        String doubled = goal + goal;
-        if(doubled.contains(s)){
-            return true;
+        StringBuilder sb = new StringBuilder(s);
+        for(int i=0;i<s.length();i++){
+            sb.append(s.charAt(i));
+            sb.deleteCharAt(0);
+            if((sb.toString()).equals(goal)){
+                return true;
+            }
         }
+        // String doubled = goal + goal;
+        // if(doubled.contains(s)){
+        //     return true;
+        // }
         return false;
     }
 }
